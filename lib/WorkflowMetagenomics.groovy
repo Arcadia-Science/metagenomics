91eef55c836e4d2b9465477cb0a1dbb7ed3dad8d
elizabethmcd <elizabethmcd93@gmail.com>
2022-10-14 13:29:28 -0700

    remove trailling whitespace giving error

--- lib/WorkflowMetagenomics.groovy
+++ lib/WorkflowMetagenomics.groovy
@@ -10,7 +10,6 @@ class WorkflowMetagenomics {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        
 
         if (!params.fasta) {
             log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
